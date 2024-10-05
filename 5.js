522cb024f65ed61427efaea934466023300a70e9
shelbyclarice <shelbyclarice@gmail.com>
2024-10-05 12:20:35 -0400

    Middle of for..of

--- 5.js
+++ 5.js
@@ -62,3 +62,64 @@ const animals = [ 'lions', 'tigers', 'bears' ];
 for (let i = 0; i < animals.length; i++) {
     console.log(i, animals[i]);
 } // 0 'lions' // 1 'tigers' // 2 'bears'
+
+// While Loops (continues to run as long as its test condition is true)
+let num = 0;
+while (num < 10) {
+    console.log(num);
+    num++;
+} // get 0 - 9
+
+// prefers a for loop in this situation so that you don't have to seperate, better practice when you don't have to create a seperate variable outside of a loop
+for(let i = 0; i<=5; i++){
+    console.log(i)
+} // get 0 - 5
+// Turning the above to a while loop
+let j = 0;
+while(j <= 5){
+    console.log(j);
+    j++;
+} // 0 - 5, same output
+
+// Benefit of a while loop is not knowing how long it's going to run
+// Ex) chess game - continues until there is a game over
+const target = Math.floor(Math.random() * 10); // Math.random() - Gives a random number between 0 and 1 // Math.floor() rounds a number down to the nearest integer
+let guess = Math.floor(Math.random() * 10);
+while(guess !== target){
+    console.log(guess);
+    guess = Math.floor(Math.random() * 10);
+}
+console.log(`Target: ${target} Guess: ${guess}`); // there is a target number, a random number is generated and will show the Target number and the Guess number. Will run until the numbers match, then the while(guess !== target) will be false
+
+// while(some condition)
+// in the loop, update or attempt to make that condition false
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    if (i === 5) {
+        break; // Will stop running once it reaches 5 // Uncommon in for loops
+    }
+}
+// More common in while loops, good to break out of an infinite loop
+while (true) {
+    if (target === guess) break; // will stop once target equals guess
+}
+
+// For...Of (Iterating over arrays)
+for (variable of iterable) {
+    statment
+}
+
+let subreddits = [ 'soccor', 'popheads', 'cringe', 'books' ];
+// For loop
+for(let i = 0; i < subreddits.length; i++){
+    console.log(subreddits[i]);
+}
+// Changing it to for..up
+for(let sub of subreddits){ // sub = sub elements
+    console.log(sub);
+}
+
+for (let char of 'cockadoodledoo'){ // char = character
+    console.log(char.toUpperCase());
+}
